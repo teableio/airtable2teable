0512b82a226e82f974dab19de555ed02989e4bc1
Itou Ng <13060889537@sina.cn>
2023-09-07 22:06:40 +0800

    style: prisma DbExecute create table.

--- test/prisma.model.define.test.ts
+++ test/prisma.model.define.test.ts
@@ -1,12 +1,7 @@
 import { DbExecute } from '@prisma/migrate';
 
 describe('Prisma create model', () => {
-  test('demo', async () => {
-    // const prisma = new PrismaClient({
-    //   datasourceUrl: `file:./test.db`,
-    // });
-    // prisma.$executeRaw`CREATE TABLE test(ID INT PRIMARY KEY  NOT NULL)`;
-    // const connect = prisma.$connect();
+  test('db execute', async () => {
     const result = await DbExecute.new().parse([
       `--url=file:${__dirname}/test.db`,
       `--file=${__dirname}/script.sql`,
--- test/script.sql
+++ test/script.sql
@@ -1,3 +1,4 @@
 -- Drop & Create & Drop
 DROP TABLE IF EXISTS 'test-dbexecute';
 CREATE TABLE 'test-dbexecute' ("id" INTEGER PRIMARY KEY);
+DROP TABLE IF EXISTS 'test-dbexecute';

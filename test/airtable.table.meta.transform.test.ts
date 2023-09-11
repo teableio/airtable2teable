6b6c2e9a19b0f175694da07fb589a844171e74ae
Itou Ng <13060889537@sina.cn>
2023-09-11 21:37:00 +0800

    test: store table field

--- test/airtable.table.meta.transform.test.ts
+++ test/airtable.table.meta.transform.test.ts
@@ -33,6 +33,7 @@ describe('airtable table meta transform', () => {
       data: {
         id: airtableTable.id,
         name: airtableTable.name,
+        description: airtableTable.description,
         db_table_name: 'visual_xxx',
         version: 1,
         order: 1,
@@ -43,9 +44,46 @@ describe('airtable table meta transform', () => {
       },
     });
     expect(tableMeta).toBeDefined();
-    const { count } = await client.table_meta.deleteMany({
+    for (const field of airtableTable.fields) {
+      await client.field.create({
+        data: {
+          id: field.id,
+          name: field.name,
+          description: field.description,
+          options: '{}',
+          type: field.type,
+          cell_value_type: 'string',
+          is_multiple_cell_value: false,
+          db_field_type: 'TEXT',
+          db_field_name: `unnamed_${field.id}`,
+          not_null: false,
+          unique: false,
+          is_primary: field.id === airtableTable.primaryFieldId,
+          is_computed: false,
+          is_lookup: false,
+          has_error: false,
+          lookup_linked_field_id: null,
+          lookup_options: null,
+          table_id: airtableTable.id,
+          column_meta: '{}',
+          version: 1,
+          created_time: new Date(),
+          last_modified_time: new Date(),
+          deleted_time: null,
+          created_by: 'admin',
+          last_modified_by: 'admin',
+        },
+      });
+    }
+    const count = await client.field.count();
+    expect(count).toEqual(1);
+    const deletedField = await client.field.delete({
+      where: { id: airtableTable.primaryFieldId },
+    });
+    expect(deletedField).toBeDefined();
+    const deletedTable = await client.table_meta.delete({
       where: { id: tableMeta.id },
     });
-    expect(count).toEqual(1);
+    expect(deletedTable).toBeDefined();
   });
 });

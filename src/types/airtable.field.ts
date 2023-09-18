be6ab60ac4b95e650c31874b4db84ecbde5f9405
Itou Ng <13060889537@sina.cn>
2023-09-18 21:31:40 +0800

    fix: all airtable field model transform to teable text field model.

--- src/types/airtable.field.ts
+++ src/types/airtable.field.ts
@@ -1,5 +1,12 @@
+import { plainToInstance } from 'class-transformer';
+
+import { TeableSingleLineTextField } from '../models';
 import { AirtableFieldTypeEnum } from './airtable.field.type.enum';
 import { IAirtableField } from './fields';
+import { TeableCellValueType } from './teable.cell.value.type';
+import { TeableDbFieldType } from './teable.db.field.type';
+import { TeableField } from './teable.field';
+import { TeableFieldType } from './teable.field.type.enum';
 
 export abstract class AirtableField {
   protected constructor(private readonly field: IAirtableField) {}
@@ -16,5 +23,23 @@ export abstract class AirtableField {
     return this.field.type;
   }
 
+  get description(): string {
+    return this.description;
+  }
+
   abstract getCellValue(any): any;
+
+  transformDataModel(): TeableField {
+    const json = {
+      id: this.id,
+      name: this.name,
+      description: this.description,
+      type: TeableFieldType.SingleLineText,
+      dbFieldType: TeableDbFieldType.Text,
+      options: {},
+      cellValueType: TeableCellValueType.String,
+      isComputed: false,
+    };
+    return plainToInstance(TeableSingleLineTextField, json);
+  }
 }

a0880c3c9c01c803a7c8861b1466b9bc7d7a8441
Itou Ng <13060889537@sina.cn>
2024-10-02 20:47:35 +0800

    feat: ui

--- apps/nextjs-app/src/pages/index.tsx
+++ apps/nextjs-app/src/pages/index.tsx
@@ -1,115 +1,76 @@
-import Image from "next/image";
-import localFont from "next/font/local";
+import * as React from "react"
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="w-full h-screen flex items-center justify-center">
+      <Card className="w-[700px]">
+        <CardHeader>
+          <CardTitle>Airtable to Teable</CardTitle>
+          <CardDescription>Migrate data from airtable.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-4">
+            <div className="flex flex-row space-x-4">
+              <div className="grid basis-1/2 items-center">
+                <div className="flex flex-col space-y-2">
+                  <Label htmlFor="airtable-token">Airtable Token</Label>
+                  <Input id="airtable-token" placeholder="AIRTABLE_TOKEN" />
+                </div>
+              </div>
+              <div className="grid basis-1/2 items-center">
+                <div className="flex flex-col space-y-2">
+                  <Label htmlFor="airtable-base-id">Airtable Base ID</Label>
+                  <Input id="airtable-base-id" placeholder="AIRTABLE_BASE_ID" />
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row space-x-4">
+              <div className="grid basis-1/2 items-center">
+                <div className="flex flex-col space-y-2">
+                  <Label htmlFor="teable-token">Teable Token</Label>
+                  <Input id="teable-token" placeholder="TEABLE_TOKEN" />
+                </div>
+              </div>
+              <div className="grid basis-1/2 items-center">
+                <div className="flex flex-col space-y-2">
+                  <Label htmlFor="teable-space-id">Teable Space ID</Label>
+                  <Input id="teable-space-id" placeholder="TEABLE_SPACE_ID" />
+                </div>
+              </div>
+            </div>
+            <div>
+              <Accordion type="single" collapsible orientation="horizontal" dir="rtl">
+                <AccordionItem value="other-config">
+                  <AccordionTrigger className="text-zinc-500">Other Config</AccordionTrigger>
+                  <AccordionContent>
+                    <div className="flex flex-col space-y-4 text-zinc-500 px-2">
+                      <Label htmlFor="teable-base-url">Base URL</Label>
+                      <Input id="teable-base-url" placeholder="https://teable.io" />
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-row-reverse justify-between">
+          <Button>Start</Button>
+        </CardFooter>
+      </Card>
     </div>
-  );
+
+  )
 }
